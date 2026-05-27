0f658e465f8b441317225f4b707d0c01915ec384
Devansh Gupta <67866743+Devansh-bit@users.noreply.github.com>
2026-05-27 10:02:18 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
+        let mut res = vec![0i32; temperatures.len()];
+        let n = temperatures.len();
+        for i in (0..n.saturating_sub(1)).rev() {
+            let mut j = i + 1;
+            while j < n && temperatures[j] <= temperatures[i] {
+                if res[j] == 0 {
+                    j = n;
+                    break;
+                }
+                j = res[j] as usize + j;
+            }
+            if j < n {
+                res[i] = (j - i) as i32;
+            }
+        }
+        res
+    }
+}

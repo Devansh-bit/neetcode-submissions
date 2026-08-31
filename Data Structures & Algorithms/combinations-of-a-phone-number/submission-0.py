5ccedf510229c4df50f9db0b9e999984baff8d2e
Devansh Gupta <67866743+Devansh-bit@users.noreply.github.com>
2026-08-31 14:56:10 +0530

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.py
@@ -0,0 +1,28 @@
+representation: dict[str, list[str]] = {
+    "2": ["a", "b", "c"],
+    "3": ["d", "e", "f"],
+    "4": ["g", "h", "i"],
+    "5": ["j", "k", "l"],
+    "6": ["m", "n", "o"],
+    "7": ["p", "q", "r", "s"],
+    "8": ["t", "u", "v"],
+    "9": ["w", "x", "y", "z"],
+}
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        result = []
+
+        def explore(index, path):
+            if index == len(digits):
+                result.append(path)
+                return
+            
+            current_digit = digits[index]
+            for letter in representation[current_digit]:
+                explore(index+1, path+letter)
+        
+        explore(0, "")
+        return result

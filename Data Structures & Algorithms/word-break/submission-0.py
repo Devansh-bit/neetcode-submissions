17f182c61e0d509c7516cfcefcf5483265b32f0f
Devansh Gupta <67866743+Devansh-bit@users.noreply.github.com>
2026-09-23 15:00:09 +0530

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        word_set = set(wordDict)
+        max_len = max(len(w) for w in wordDict)
+        
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+        
+        for i in range(1, len(s) + 1):
+            # Only look back as far as the longest word in the dictionary
+            for j in range(max(0, i - max_len), i):
+                if dp[j] and s[j:i] in word_set:
+                    dp[i] = True
+                    break
+                    
+        return dp[len(s)]
